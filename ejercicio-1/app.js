9ce52225b49b2b9ed41925a908c69ebf0a367fc1
omiras <oscar.miras@gmail.com>
2024-06-11 11:23:09 +0200

    sol

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const jsonFile = path.join(__dirname, 'hacienda.json');
+let dni = "76543210D";
 
 const hacienda = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
 
-for (person of hacienda) {
-    console.log(person.nombre);
+let found = false;
+
+let i = 0;
+while (!found && i < hacienda.length) {
+    found = hacienda[i].dni == dni;
+    if (found) {
+        console.log(`El/la contribuyete ${hacienda[i].nombre} tiene noficaciones pendientes. Se enviará un mail a ${hacienda[i].email}`)
+    }
+    i++;
 }
+
