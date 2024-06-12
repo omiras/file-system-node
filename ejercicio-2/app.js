68f4e2c58d656f60e3ee49d381f46da71b36cd2c
omiras <oscar.miras@gmail.com>
2024-06-12 09:54:40 +0200

    done

--- ejercicio-2/app.js
+++ ejercicio-2/app.js
@@ -4,5 +4,34 @@ const fs = require('fs');
 // Cargo el módulo interno 'path' para construir rutas de forma más sencilla`
 const path = require('path');
 
-// Construye una ruta 
-const pathToFile = path.join(__dirname, "files", "2abril_participants_94849208356.csv");
+
+function readFileCSV(pathToFile) {
+    // Leemos el fichero 
+    fs.readFile(pathToFile, "utf-8", (err, data) => {
+        // Obtenemos cada una de las lineas por separado
+        const parsedData = data.split('\r\n');
+        const pathToReportFile = path.join("files", "informe.txt");
+
+        fs.appendFileSync(pathToReportFile, `\nSiguiente fichero analizado: ${pathToFile}\n\n`);
+
+
+        // iteramos sobre cada elemento del array
+        for (line of parsedData) {
+            const infoEntry = line.split(',');
+            const message = `${infoEntry[0]} ha venido a clase durante ${infoEntry[2]} minutos.\n`;
+
+            fs.appendFileSync(pathToReportFile, message);
+        }
+
+    });
+
+}
+
+const files = fs.readdirSync("files");
+
+for (const file of files) {
+    // Construye una ruta relativa al directorio actual.
+    const pathToFile = path.join("files", file);
+    readFileCSV(pathToFile);
+}
+
