ae5070f5884fdc3782aa4a42f110ce49eb041cc9
VanyaMark <markova.vanya@gmail.com>
2024-06-15 22:57:19 +0200

    iteration 3 done ex 1

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,12 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-let dni = "";
+const fs = require("fs");
+const path = require("path");
+let dni = "76543210D";
 
 /** Leer README */
 
-fs.readFile("./hacienda.json", "utf-8", (err, data) =>{
-    const parsedData = JSON.parse(data);
-    const namesArr = parsedData.forEach(person => {
-        console.log(person.nombre)
-    });
-})
+// fs.readFile("./hacienda.json", "utf-8", (err, data) => {
+//   const parsedData = JSON.parse(data);
+//   const namesArr = parsedData.forEach((person) => {
+//     console.log(person.nombre);
+//   });
+// });
+
+const hacienda = JSON.parse(fs.readFileSync('./hacienda.json', 'utf8'));
+
+let found = false;
+
+let i = 0;
+while (!found && i < hacienda.length) {
+    found = hacienda[i].dni == dni && hacienda[i].notificacionesPendientes;
+    if (found) {
+        const message = `El/la contribuyente ${hacienda[i].nombre} tiene notificaciones pendientes. Se enviará un mail a ${hacienda[i].email}\n`;
+        // fs.appendFileSync("notificaciones.txt", message);
+        console.log(message)
+    }
+    i++;
+}
