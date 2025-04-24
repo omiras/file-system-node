89331e7caf6bc882a542ef5326c66579bd7aac2b
omiras <oscar.miras@gmail.com>
2025-04-24 21:59:35 +0200

    done

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,6 +1,36 @@
 const fs = require('fs');
 const path = require('path');
-let dni = "";
+const logDIR = "logs";
+let dni = "76543210D";
 
 /** Leer README */
+
+
+const fileContents = fs.readFileSync("hacienda.json", "utf-8");
+
+const parsedData = JSON.parse(fileContents);
+
+// Corregir Iteración 2 a las 21.11
+
+// https://www.w3schools.com/js/js_loop_forof.asp
+for (const person of parsedData) {
+    console.log(person.nombre);
+}
+
+// Iteración 3: 21.35
+let personaEncontrada;
+let i = 0;
+
+while (!personaEncontrada && i<parsedData.length) {
+    if (parsedData[i].dni == dni) {
+       personaEncontrada = parsedData[i];
+    }
+    i++;
+}
+
+if (personaEncontrada && personaEncontrada.notificacionesPendientes) {
+    const message = `El/La contribuyente ${personaEncontrada.nombre} tiene notificaciones pendientes. Se enviará un correo a ${personaEncontrada.email}\n`; 
+    fs.appendFileSync(path.join(__dirname, logDIR, "notificaciones.txt"), message);
+
+}
 
